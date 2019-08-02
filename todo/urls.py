4bfe924e63ec83bdcd73f56f9035106e8779cafc
Jst <jsthtet96@gmail.com>
2019-08-02 21:35:48 +0630

    updating

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     path('todo/', views.todo_list),
     path('todo/<int:pk>/', views.todo_detail),
-]
+])
