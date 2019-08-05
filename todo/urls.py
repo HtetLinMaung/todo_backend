97c2a98469c25871c3cf434d6a38aeba7f6ea2f5
htet_lin_maung <htet.lin.maung.@fusionsol.com>
2019-08-05 10:16:04 +0630

    saving

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = format_suffix_patterns([
-    path('todo/', views.todo_list),
-    path('todo/<int:pk>/', views.todo_detail),
+    path('todo/', views.TodoList.as_view()),
+    path('todo/<int:pk>/', views.TodoDetail.as_view()),
 ])
--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clear_cache',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'todo.apps.TodoConfig',
-    'corsheaders'
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
